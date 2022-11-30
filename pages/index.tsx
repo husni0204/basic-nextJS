66ef007b1744a4e722b93f30cfe5a2a7b37c9aef
husni0204 <husnisale@gmail.com>
2022-11-30 16:34:21 +0700

    test ssh

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
-import type { NextPage } from 'next'
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-      <Layout pageTitle="Home Page">
-        <h1 className={styles['title-homepage']}>Welcome Remira Studio</h1>
-      </Layout>
-  )
-}
+    <Layout pageTitle="Home Page">
+      <h1 className={styles["title-homepage"]}>Welcome Remira Studio</h1>
+      <p>Test koneksi SSH</p>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
